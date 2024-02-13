3ffd743c448479818d3c698dd5b6591584a7d02d
Max Walden <mxwaldo@Maxs-MacBook-Pro-10.local>
2024-02-13 00:34:56 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We will attempt to use ML for neuronal decoding via a XGBoost and NN ensemble on the Neuropixels - Visual coding dataset.',
+    author='team-aardvark',
+    license='MIT',
+)
